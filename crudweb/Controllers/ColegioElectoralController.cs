a9fdbad9c440aad39989b39bed3ac5dde5689a15
spinales <saul021002@gmail.com>
2022-03-30 18:13:51 -0400

    update

--- crudweb/Controllers/ColegioElectoralController.cs
+++ crudweb/Controllers/ColegioElectoralController.cs
@@ -36,14 +36,6 @@ public class ColegioElectoralController : Controller
         ViewData["Sectors"] = new SelectList(Sectors,"Id","Nombre");
         return View();
     }
- 
-    public IActionResult ViewBags()
-    {
-        ViewBag.Municipios = _context.Municipios.ToList();
-        ViewBag.Sectors = _context.Sectors.ToList();
-
-        return View();
-    }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
@@ -53,15 +45,15 @@ public class ColegioElectoralController : Controller
         try
         {
             if (ModelState.IsValid)
-            {
+            {   
+                colegio.FechaCreacion = DateTime.Now;
                 _context.Add(colegio);
                 _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
         }
         catch (DbUpdateException ex)
-        {
-            
+        {   
             ModelState.AddModelError("", "Unable to save changes. " +
             "Try again, and if the problem persists " +
             "Contact me :)");
@@ -97,5 +89,55 @@ public class ColegioElectoralController : Controller
     public IActionResult Error()
     {
         return View("Error!");
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        var Municipios = _context.Municipios.ToList();
+        ViewData["Municipios"] = new SelectList(Municipios,"Id","Nombre");
+        var Sectors = _context.Sectors.ToList();
+        ViewData["Sectors"] = new SelectList(Sectors,"Id","Nombre");
+
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var colegio = _context.ColegioElectorals.Find(id);
+        if (colegio == null)
+        {
+            return NotFound();
+        }
+
+        return View(colegio);
+    }
+
+    [HttpPost, ActionName("Edit")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> EditPost(int? id, 
+    [Bind("Nombre,Descripcion,Direccion,Telefono,MunicipioId,SectorId")] ColegioElectoral colegio)
+    {
+        if (id == colegio.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(colegio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException ex)
+            {
+                //Log the error (uncomment ex variable name and write a log.)
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists, " +
+                    "Contact Me :).");
+            }
+        }
+        return View(colegio);
     }
 }
