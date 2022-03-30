1cd96ba7e4cf0850a8d3fcf61cf68cbd43403142
spinales <saul021002@gmail.com>
2022-03-30 17:46:03 -0400

    agregar

--- crudweb/Controllers/ColegioElectoralController.cs
+++ crudweb/Controllers/ColegioElectoralController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using crudweb.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace crudweb.Controllers;
 
@@ -23,6 +25,49 @@ public class ColegioElectoralController : Controller
     public IActionResult Index()
     {
         return View(_context.ColegioElectorals.ToList());
+    }
+
+    // GET: ColegioElectoral/Create
+    public IActionResult Create()
+    {
+        var Municipios = _context.Municipios.ToList();
+        ViewData["Municipios"] = new SelectList(Municipios,"Id","Nombre");
+        var Sectors = _context.Sectors.ToList();
+        ViewData["Sectors"] = new SelectList(Sectors,"Id","Nombre");
+        return View();
+    }
+ 
+    public IActionResult ViewBags()
+    {
+        ViewBag.Municipios = _context.Municipios.ToList();
+        ViewBag.Sectors = _context.Sectors.ToList();
+
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(
+        [Bind("Codigo,Nombre,Descripcion,Direccion,Telefono,MunicipioId,SectorId,Estado")] ColegioElectoral colegio)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(colegio);
+                _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+        }
+        catch (DbUpdateException ex)
+        {
+            
+            ModelState.AddModelError("", "Unable to save changes. " +
+            "Try again, and if the problem persists " +
+            "Contact me :)");
+        }
+
+        return View(colegio);
     }
 
     public IActionResult Details(int? id)
